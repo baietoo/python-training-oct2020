11657bd75962566b70f2bc852477f59f6536c341
CATALIN GHERASIM CIRCU <X200326@arpege.socgen>
2020-10-26 16:39:34 +0200

    fizz buzz and driver problems edit

--- circu_day1.py
+++ circu_day1.py
@@ -72,13 +72,13 @@ else:
 
     # Fizz Buzz
     for i in range(1, 50):
-        if i % 3 == 0 and i % 5 == 0:
+        if i == 30:
+            break
+        elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         elif i % 3 == 0:
             print("Fizz")
         elif i % 5 == 0:
             print("Buzz")
-        elif i == 30:
-            break
         else:
             print(i)
