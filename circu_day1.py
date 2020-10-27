6486b20cae7a5894eadf01abb1e192c78b1dd9bd
CATALIN GHERASIM CIRCU <X200326@arpege.socgen>
2020-10-27 12:13:22 +0200

    lists day2

--- circu_day1.py
+++ circu_day1.py
@@ -72,13 +72,11 @@ else:
 
     # Fizz Buzz
     for i in range(1, 50):
+        result = ''
         if i == 30:
             break
-        elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+        if i % 3 == 0:
+            result += "Fizz"
+        if i % 5 == 0:
+            result += "Buzz"
+        print(result or i)
